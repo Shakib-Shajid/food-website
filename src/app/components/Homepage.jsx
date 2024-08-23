e33a0d259f35c12920caa08195b48b3d20a68bcc
Shakib-Shajid <shakibalshajid1682@gmail.com>
2024-08-24 03:00:09 +0600

    text added with fire

---
+++ src/app/components/Homepage.jsx
@@ -0,0 +1,35 @@
+"use client"
+
+import Image from "next/image";
+import { useState } from "react";
+
+const Homepage = () => {
+
+    const [isHover, setIsHover] = useState(false);
+
+    const mouseEnter = () => { setIsHover(true) }
+    const mouseLeave = () => { setIsHover(false) }
+
+    return (
+        <div className="relative" onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>
+            {
+                isHover ? <div>
+                    <Image src="/fire.gif" width="30" height="30" alt="" className="absolute -top-5 " />
+                    <Image src="/fire.gif" width="30" height="30" alt="" className="absolute bottom-4 left-[102px]" />
+                    <Image src="/fire.gif" width="30" height="30" alt="" className="absolute -top-2 left-[170px]" />
+                    <Image src="/fire.gif" width="30" height="30" alt="" className="absolute top-8 left-[344px]" />
+                    <h1 className="text-9xl font-extrabold ">VICIO CITIES</h1>
+                    <Image src="/fire.gif" width="30" height="30" alt="" className="absolute right-[356px] bottom-8" />
+                    <Image src="/fire.gif" width="30" height="30" alt="" className="absolute right-52 bottom-[106px]" />
+                    <Image src="/fire.gif" width="30" height="30" alt="" className="absolute right-40 bottom-8" />
+                    <Image src="/fire.gif" width="30" height="30" alt="" className="absolute right-[92px] bottom-[68px]" />
+                    <Image src="/fire.gif" width="30" height="30" alt="" className="absolute right-0 bottom-7" />
+                </div> : <h1 className="text-9xl font-extrabold ">VICIO CITIES</h1>
+            }
+        </div>
+    );
+};
+
+export default Homepage;
+
+
--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,13 @@
 import Image from "next/image";
+import Homepage from "./components/Homepage";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h3>Create food website</h3>
+        <Homepage />
     </main>
   );
 }
 
 
-
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Shakib-Shajid/food-website.git
-git push -u origin main
