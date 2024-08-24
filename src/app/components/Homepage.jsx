b5196cf49f690d8744737850b2ed247b2838736f
Shakib-Shajid <shakibalshajid1682@gmail.com>
2024-08-24 18:23:48 +0600

    burger image added with arrow

--- src/app/components/Homepage.jsx
+++ src/app/components/Homepage.jsx
@@ -2,6 +2,8 @@
 
 import Image from "next/image";
 import { useState } from "react";
+import { TfiWorld } from "react-icons/tfi";
+import { FiArrowUpRight } from "react-icons/fi";
 
 const Homepage = () => {
 
@@ -11,25 +13,32 @@ const Homepage = () => {
     const mouseLeave = () => { setIsHover(false) }
 
     return (
-        <div onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} className="h-20">
+        <div onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} className="h-20 mt-10">
+            <small className="flex gap-3 items-center relative -bottom-3"><TfiWorld className="text-xl" /><TfiWorld className="absolute left-2 flex items-baseline text-xl" /><span>VICIO BURGER CLUB</span></small>
             {
-                isHover ? <div className="border border-red-600 relative">
-                    <Image src="/fire.gif" width="30" height="30" alt="" className="absolute -top-5 " />
-                    <Image src="/fire.gif" width="30" height="30" alt="" className="absolute bottom-4 left-[102px]" />
-                    <Image src="/fire.gif" width="30" height="30" alt="" className="absolute -top-2 left-[170px]" />
-                    <Image src="/fire.gif" width="30" height="30" alt="" className="absolute top-8 left-[344px]" />
+                isHover ? <div className="relative">
+                    <Image src="/fire.gif" width="24" height="24" alt="" className="absolute -top-3 " />
+                    <Image src="/fire.gif" width="24" height="24" alt="" className="absolute bottom-4 left-[102px]" />
+                    <Image src="/fire.gif" width="24" height="24" alt="" className="absolute -top-1 left-[174px]" />
+                    <Image src="/fire.gif" width="24" height="24" alt="" className="absolute top-8 left-[344px]" />
                     <h1 className="text-9xl font-extrabold ">VICIO CITIES</h1>
-                    <Image src="/fire.gif" width="30" height="30" alt="" className="absolute right-[356px] bottom-8" />
-                    <Image src="/fire.gif" width="30" height="30" alt="" className="absolute right-52 bottom-[106px]" />
-                    <Image src="/fire.gif" width="30" height="30" alt="" className="absolute right-40 bottom-8" />
-                    <Image src="/fire.gif" width="30" height="30" alt="" className="absolute right-[92px] bottom-[68px]" />
-                    <Image src="/fire.gif" width="30" height="30" alt="" className="absolute right-0 bottom-7" />
-                </div> : <h1 className="text-9xl font-extrabold ">VICIO CITIES</h1>
+                    
+                    <Image src="/fire.gif" width="24" height="24" alt="" className="absolute right-[356px] bottom-8" />
+                    <Image src="/fire.gif" width="24" height="24" alt="" className="absolute right-52 bottom-[106px]" />
+                    <Image src="/fire.gif" width="24" height="24" alt="" className="absolute right-40 bottom-8" />
+                    <Image src="/fire.gif" width="24" height="24" alt="" className="absolute right-[92px] bottom-[70px]" />
+                    <Image src="/fire.gif" width="24" height="24" alt="" className="absolute right-0 bottom-7" />
+
+                    <FiArrowUpRight className="absolute rotate-90 text-2xl bottom-3 " />
+
+                </div> : <div className="relative ">
+                    <h1 className="text-9xl font-extrabold">VICIO CITIES</h1>
+                    <FiArrowUpRight className="absolute text-2xl bottom-3 " />
+                </div>
             }
         </div>
     );
 };
 
 export default Homepage;
-
 
--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center ">
       <div className="flex gap-2">
-        <Image src="/burger.png" alt="" width="700" height="700" />
+        <Image src="/burger.png" className="hover:scale-95 translate-transform duration-500" alt="" width="700" height="700" />
         <Homepage />
       </div>
     </main>
