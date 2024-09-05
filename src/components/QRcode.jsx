3487fea34bcb2269b2a03028f4e27786e037e175
Shakib-Shajid <shakibalshajid1682@gmail.com>
2024-09-06 03:00:35 +0600

    place added

--- src/components/QRcode.jsx
+++ src/components/QRcode.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { FaInstagram } from "react-icons/fa6";
 import { MdArrowOutward } from "react-icons/md";
 
@@ -6,15 +7,17 @@ import { MdArrowOutward } from "react-icons/md";
 const QRcode = () => {
     return (
         <div>
-            <div className="relative -left-56">
-                <h3 className="text-[10px] text-end font-bold">DAILY DOSE <br />
-                    OF VICIO HERE</h3>
-                <Image src="/qr.png" width="70" height="70" alt="" className="mx-auto" />
-                <div className="flex items-baseline group">
-                    <MdArrowOutward className="text-4xl transform transition-transform duration-500 group-hover:rotate-90" />
-                    <FaInstagram className="text-3xl transform transition-transform duration-500 group-hover:-rotate-180" />
+            <Link href="https://www.instagram.com/" className="group" target="_blank">
+                <div className="relative -left-56">
+                    <h3 className="text-[10px] text-end font-bold">DAILY DOSE <br />
+                        OF VICIO HERE</h3>
+                    <Image src="/qr.png" width="70" height="70" alt="" className="mx-auto" />
+                    <div className="flex items-baseline group">
+                        <MdArrowOutward className="text-4xl transform transition-transform duration-500 group-hover:rotate-90" />
+                        <FaInstagram className="text-3xl transform transition-transform duration-500 group-hover:-rotate-180" />
+                    </div>
                 </div>
-            </div>
+            </Link>
         </div>
     );
 };
