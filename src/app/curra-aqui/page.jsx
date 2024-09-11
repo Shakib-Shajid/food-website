a90045f50f1bb39137fd02eff5511832e665897c
Shakib-Shajid <shakibalshajid1682@gmail.com>
2024-09-11 17:20:21 +0600

    arrow and text added

--- src/app/curra-aqui/page.jsx
+++ src/app/curra-aqui/page.jsx
@@ -7,7 +7,7 @@ import { GoArrowUpLeft } from "react-icons/go";
 
 const page = () => {
     return (
-        
+
         <div className="ml-5 flex">
             <h2 className="text-5xl font-bold uppercase fixed">EL INGREDIENTE <br />
                 Principal.</h2>
@@ -63,24 +63,7 @@ const page = () => {
                 </div>
 
             </div>
-            <div className="flex-shrink-0">
-                <div className=" mt-36 mr-0">
-                    <Image src="/arrow_1.png" width="500" height="10" alt="" className="w-full" />
-                </div>
-            </div>
-
-            <div className="flex-shrink-0 w-screen bg-black text-white p-40 ">
-
-                <div className="uppercase font-bold text-xl relative space-y-3">
-                    <Image src="/people.png" width="100" height="100" alt="" className="bg-white absolute left-[300px] -mt-1" />
-                    <p>SI HAS LLEGADO HASTA AQUí</p>
-                    <p>Y QUIERES FORMAR PARTE DE<span className="inline-block">............... ESTO</span></p>
-                    <p>PUEDES VER AQUÍ LAS <Link href="/curra-aqui" className="text-red-500 hover:underline">OPORTUNIDADES DE EMPLEO</Link><GoArrowUpLeft className="inline-block ml-1" /></p>
-                    <p>O INCLUSO ESCRIBIRNOS UN <Link href="mailto:talent@ganasdevicio.com?subject=Ganas%20de%20currar%20en%20Vicio" className="text-red-500 hover:underline">MAIL</Link><GoArrowUpLeft className="inline-block ml-1" /></p>
-                    <p className="text-3xl">O Simplemente USAR EL CAMINO <span className="text-red-500">Rápido</span>.</p>
-
-                </div>
-            </div>
+            
 
         </div>
 
